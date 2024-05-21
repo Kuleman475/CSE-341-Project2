bf1cdf09d3c0502d1a2b5a8e23f0891ef34907c6
Taylor Christensen <97205707+Kuleman475@users.noreply.github.com>
2024-05-20 21:10:55 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var mongodbClient = require('mongodb');
+var mongodb = require('./db/connect');
+const swaggerAutogen = require('swagger-autogen')();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+var app = express();
+const port = process.env.PORT || 8888;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port} :) and is connected to DataBase :)`);
+    });
+  }
+});
+
