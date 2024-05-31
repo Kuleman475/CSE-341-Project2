8167984b10011525ecc6993b32f5b51316c7a42f
Taylor Christensen <97205707+Kuleman475@users.noreply.github.com>
2024-05-31 18:54:03 -0400

    5

--- controller/users.js
+++ controller/users.js
@@ -5,7 +5,8 @@ const objectId = require('mongodb').ObjectId;
 const getAllData = async (req, res, next) => {
   const userInfo = await mongodb.getDb().db('Films').collection('user').find()
   userInfo.toArray().then((list) => {
-    console.log(userInfo);
+    console.log(userInfo, "user info");
+    console.log(list, "list");
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(list);
   });
