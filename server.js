f04a823dcf587fa56cd82cb25ac3a63648e20436
Use your GitHub username <Enter your email address>
2024-06-04 20:49:59 -0400

    oauth complete

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 const swaggerAutogen = require('swagger-autogen')();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const axios = require('axios');
+require('dotenv').config();
 
 var app = express();
 const port = process.env.PORT || 8888;
@@ -31,3 +33,37 @@ mongodb.initDb((err, mongodb) => {
     });
   }
 });
+
+app.get('/auth', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+  );
+});
+
+
+app.get('/oauth-callback', ({ query: { code } }, res) => {
+  const body = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_SECRET,
+    code,  
+  };
+
+const opts = { headers: { accept: 'application/json' } };
+
+    axios
+    .post('https://github.com/login/oauth/access_token', body, opts)
+    .then((response) => response.data.access_token) //response.data.access_token)
+    .then((token) => {
+      // eslint-disable-next-line no-console
+      console.log('My token:', token);
+
+      if(token != "undefined"){
+        res.redirect(`/?token=${token}`)//.json("Congratulations you are authorized")
+      }
+    
+      
+    })
+    .catch((err) => res.status(500).json({ err: err.message }));
+});
+
+
